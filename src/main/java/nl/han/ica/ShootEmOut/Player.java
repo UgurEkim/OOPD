d2708a382448db7abd9f471acd657adf2e5179ab
Leon Chen <leonchen@live.nl>
2016-10-18 21:00:36 +0200

    Player movement gefixt en alarm aangezet

--- src/main/java/nl/han/ica/ShootEmOut/Player.java
+++ src/main/java/nl/han/ica/ShootEmOut/Player.java
@@ -1,6 +1,8 @@
 package nl.han.ica.ShootEmOut;
 
 import java.util.List;
+
+import nl.han.ica.OOPDProcessingEngineHAN.Alarm.Alarm;
 import nl.han.ica.OOPDProcessingEngineHAN.Alarm.IAlarmListener;
 import nl.han.ica.OOPDProcessingEngineHAN.Collision.ICollidableWithGameObjects;
 import nl.han.ica.OOPDProcessingEngineHAN.Objects.GameObject;
@@ -8,24 +10,35 @@ import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
 import nl.han.ica.OOPDProcessingEngineHAN.Objects.SpriteObject;
 
 public class Player extends SpriteObject implements ICollidableWithGameObjects, IAlarmListener {
-
-	private float attackInterval;
+	private Alarm alarm;
+	private int speed;
+	private double attackInterval;
 	private int attackAmount;
 	private int attackDamage;
 	private Health health;
 	private Powerup[] powerup;
 	private ShootEmOut SEO;
+	
+	private boolean canShoot;
+	private boolean leftKey;
+	private boolean rightKey;
+	private boolean spaceKey;
 
 	public Player(ShootEmOut SEO, float x) {
 		super(new Sprite("src/main/java/nl/han/ica/ShootEmOut/media/player.png"));
 		this.setX(x);
 		this.setY(700);
+		this.setWidth(64);
 		this.SEO = SEO;
+		this.attackInterval = 1.0;
+		this.speed = 6;
+		resetAlarm();
 		setFriction(0F);
 	}
 
 	public void attack() {
-
+		canShoot = false; 
+    	System.out.println("pew");
 	}
 
 	public void move(int direction) {
@@ -34,33 +47,67 @@ public class Player extends SpriteObject implements ICollidableWithGameObjects, 
 	
     @Override
     public void keyPressed(int keyCode, char key) {
-        final int speed = 5;
+    	/*Je gaat dit vast niet mooi vinden ;s maar dat komt omdat bij de vorige implementatie
+    	kon je dus links/rechts gaan en spatie dan indrukken, maar niet spatie indrukken en 
+    	dan links/rechts gaan zonder dat je stopt met schieten.. dus ik probeerde om te
+    	schrijven dat left en right ook directionspeed doet als spatie is ingedrukt
+    	maar hij is dan supertraag omdat keypressed niet fatsoenlijk werkt dus daarom
+    	deze uitwerking, hij is echt heel erg vloeiend nu en werkt beter dan de vorige versie,
+    	nadeel is dat ie keilelijk nu is........ .w.*/
         if (keyCode == SEO.LEFT) {
-            setDirectionSpeed(270, speed);
-        }
-        if (keyCode == SEO.UP) {
-            setDirectionSpeed(0, speed);
+        	leftKey = true;
         }
         if (keyCode == SEO.RIGHT) {
-            setDirectionSpeed(90, speed);
-        }
-        if (keyCode == SEO.DOWN) {
-            setDirectionSpeed(180, speed);
+        	rightKey = true;
         }
         if (key == ' ') {
-            System.out.println("Spatie!");
+        	spaceKey = true;
         }
     }
 
     @Override
     public void keyReleased(int keyCode, char key) {
-    	setDirectionSpeed(0, 0);
+    	if (keyCode == SEO.LEFT) {
+        	leftKey = false;
+        }
+        if (keyCode == SEO.RIGHT) {
+        	rightKey = false;
+        }
+        if (key == ' ') {
+        	spaceKey = false;
+        }
     }
     
 	@Override
-	public void update() {
-		// TODO Auto-generated method stub
-
+	public void update() {	
+		if(rightKey){
+			setDirectionSpeed(90, speed);
+		}
+		else if(leftKey){
+			setDirectionSpeed(270, speed);
+		}
+		else{
+			setDirectionSpeed(0, 0);
+		}
+		
+		if(spaceKey && canShoot){
+			attack();
+		}
+		
+		if (getX()<0) {
+            setxSpeed(0);
+            setX(0);
+        }
+		if (getX()>SEO.getWidth() - getWidth()) {
+            setxSpeed(0);
+            setX(SEO.getWidth() - getWidth());
+        }
+	}
+	
+	private void resetAlarm(){
+		this.alarm = new Alarm("PlayerAttack", attackInterval);
+		alarm.addTarget(this);
+		alarm.start();
 	}
 
 	@Override
@@ -71,8 +118,8 @@ public class Player extends SpriteObject implements ICollidableWithGameObjects, 
 
 	@Override
 	public void triggerAlarm(String alarmName) {
-		// TODO Auto-generated method stub
-
+		canShoot = true;
+		resetAlarm();
 	}
 
 }
