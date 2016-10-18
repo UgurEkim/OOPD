1575c4ce500a6234d1f4b80ae525b81f6e109b5b
Ugur <ugur.ekim@live.nl>
2016-10-18 14:29:08 +0200

    Neccesary changes

--- src/main/java/nl/han/ica/ShootEmOut/ShootEmOut.java
+++ src/main/java/nl/han/ica/ShootEmOut/ShootEmOut.java
@@ -1,12 +1,18 @@
 package nl.han.ica.ShootEmOut;
-
 import nl.han.ica.OOPDProcessingEngineHAN.Engine.GameEngine;
+import nl.han.ica.OOPDProcessingEngineHAN.UserInput.IKeyInput;
+import processing.core.PApplet;
 
-public class ShootEmOut extends GameEngine{
 
+@SuppressWarnings("serial")
+public class ShootEmOut extends GameEngine implements IKeyInput {
+	
 	private Powerup[] powerup;
-
 	private Player player;
+	
+	public static void main(String[] args) {
+		PApplet.main("ShootEmOut");
+	}
 
 	public void setup() {
 
@@ -20,6 +26,18 @@ public class ShootEmOut extends GameEngine{
 
 	@Override
 	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(int keyCode, char key) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(int keyCode, char key) {
 		// TODO Auto-generated method stub
 		
 	}
